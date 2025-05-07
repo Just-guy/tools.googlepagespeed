df87681ad9e08eec684219e146c1ae705956c25e
Just-guy <62421873+Just-guy@users.noreply.github.com>
2025-05-07 10:38:47 +0300

    Изменил ссылки на страницу с настройками

--- install/admin/tools.googlepagespeed_options.php
+++ install/admin/tools.googlepagespeed_options.php
@@ -1,4 +1,5 @@
 <? if (is_dir($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/tools.googlepagespeed/")) {
-	// присоединяем и копируем файл
 	require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/tools.googlepagespeed/admin/tools.googlepagespeed_options.php");
+} else {
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/local/modules/tools.googlepagespeed/admin/tools.googlepagespeed_options.php");
 }
