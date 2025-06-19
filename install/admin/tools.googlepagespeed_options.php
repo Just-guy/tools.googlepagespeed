efc8f396d78b2765258452bbfcbee0b6880a4a2e
Андрей Белый <Andrej.Belyj@itb-dev.ru>
2025-06-19 16:51:14 +0300

    Changed the links to the settings page

--- install/admin/tools.googlepagespeed_options.php
+++ install/admin/tools.googlepagespeed_options.php
@@ -1,4 +1,6 @@
-<? if (is_dir($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/tools.googlepagespeed/")) {
-	// присоединяем и копируем файл
+<? if (is_dir($_SERVER["DOCUMENT_ROOT"] . "/local/modules/tools.googlepagespeed/")) {
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/local/modules/tools.googlepagespeed/admin/tools.googlepagespeed_options.php");
+} else if (is_dir($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/tools.googlepagespeed/")) {
 	require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/tools.googlepagespeed/admin/tools.googlepagespeed_options.php");
 }
+
