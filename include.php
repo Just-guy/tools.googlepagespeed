09c447a91c1f7b2762838767ece7705925c8f49b
Andrej Belyi <Andrej.Belyj@itb-dev.ru>
2026-08-20 21:25:31 +0300

    Added classes to the autoloader

--- include.php
+++ include.php
@@ -3,6 +3,9 @@
 Bitrix\Main\Loader::registerAutoloadClasses(
 	'tools.googlepagespeed',
 	array(
-		'Tools\\GooglePageSpeed\\Main'      => 'lib/Main.php',
+		'Tools\\GooglePageSpeed\\Main'                  => 'lib/Main.php',
+		'Tools\\GooglePageSpeed\\GPSOptionsTable'       => 'lib/GPSOptions.php',
+		'Tools\\GooglePageSpeed\\ConnectedCssStyleTable' => 'lib/ConnectedCssStyle.php',
+		'Tools\\GooglePageSpeed\\ConnectedJsScriptTable' => 'lib/ConnectedJsScript.php',
 	)
 );
